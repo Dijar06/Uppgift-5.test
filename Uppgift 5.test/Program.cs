68a0d148c9766020ecf1a37d153f82aa066b00c3
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 14:53:05 +0100

    Add project files.

---
+++ Uppgift 5.test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            double[] ålder = new double[5];
+            double storst = 0;
+            string äldstanamn = "";
+            for (int i = 0; i < namn.GetLength(0); i++)
+            {
+                Console.WriteLine("Skriv namn på klasskamrat " + (i + 1));
+                namn[i] = Console.ReadLine();
+                Console.WriteLine("Skriv deras ålder: ");
+                ålder[i] = double.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i < namn.GetLength(0); i++)
+            {
+                if (ålder[i] > storst)
+                {
+                    storst = ålder[i];
+                    äldstanamn = namn[i];
+                }
+            }
+            Console.WriteLine("Medelåldern är = " + (ålder[0] + ålder[1] + ålder[2] + ålder[3] + ålder[4]) / 5);
+            Console.WriteLine("Äldsta i klassen är " + äldstanamn + " som är " + storst + " år");
+        }
+    }
+}
